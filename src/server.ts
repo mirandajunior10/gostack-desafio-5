d2372da48cc1fbbe17b61e592615dfc2172ac5f9
Jorlan Souza de Miranda Júnior <jorlanjr@gmail.com>
2020-09-04 01:31:31 -0300

    fix: duplicated app

--- src/server.ts
+++ src/server.ts
@@ -1,38 +1,4 @@
-import 'reflect-metadata';
-import 'dotenv/config';
-
-import express, { Request, Response, NextFunction } from 'express';
-import 'express-async-errors';
-
-import routes from './routes';
-import AppError from './errors/AppError';
-
-import createConnection from './database';
-
-createConnection();
-
-const app = express();
-
-app.use(express.json());
-app.use(routes);
-
-app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
-  if (err instanceof AppError) {
-    return response.status(err.statusCode).json({
-      status: 'error',
-      message: err.message,
-    });
-  }
-
-  console.error(err);
-
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal server error',
-  });
-});
-
-export default app;
+import app from './app';
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
